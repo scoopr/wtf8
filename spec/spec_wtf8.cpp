6608f8629691808d265443b8763fdc9b48b9150b
Mikko Lehtonen <scoopr@iki.fi>
2011-05-16 20:43:46 +0300

    Implement decoding with implementation by Bjoern Hoehrmann. Implement specs a bit further.

--- spec/spec_wtf8.cpp
+++ spec/spec_wtf8.cpp
@@ -7,36 +7,156 @@ describe(wtf8, "decode") {
     
     it("should decode codepoint from utf8 string in the single octet range 00-7f") {
         
-        const char str1[]={ 1 };
-        const char str2[]={ 0x32 };
-        const char str3[]={ 0x7f };
-        const char str_er[]={ 0x80 };
+        const unsigned char ustr1[]={ 1 };
+        const unsigned char ustr2[]={ 0x32 };
+        const unsigned char ustr3[]={ 0x7f };
+        const unsigned char ustr_er[]={ 0x80 };
+        
+        const char* str1 = (const char*)ustr1;
+        const char* str2 = (const char*)ustr2;
+        const char* str3 = (const char*)ustr3;
+        const char* str_er = (const char*)ustr_er;
         
         unsigned int codepoint = 0;
         
         const char* res = 0;
 
         codepoint = 0;
-        res = decode_utf8(str1, 1, &codepoint);
-        should_equal( codepoint, 1);
+        res = wtf8_decode(str1, 1, &codepoint);
+        should_equal( codepoint, 1u);
         should_equal( res, str1+1 );
 
         codepoint = 0;
-        res = decode_utf8(str2, 1, &codepoint);
-        should_equal( codepoint, 0x32);
+        res = wtf8_decode(str2, 1, &codepoint);
+        should_equal( codepoint, 0x32u);
         should_equal( res, str2+1 );
 
         codepoint = 0;
-        res = decode_utf8(str3, 1, &codepoint);
-        should_equal( codepoint, 0x7f);
+        res = wtf8_decode(str3, 1, &codepoint);
+        should_equal( codepoint, 0x7fu);
         should_equal( res, str3+1 );
 
         codepoint = 0;
-        res = decode_utf8(str_er, 1, &codepoint);
-        should_equal( codepoint, 0xfffd);
+        res = wtf8_decode(str_er, 1, &codepoint);
+        should_equal( codepoint, 0xfffdu);
         should_equal( res, str_er );
         
     }
+
+    it("should decode codepoint from utf8 string in the two octet range 80-7ff") {
+        
+        const unsigned char ustr1[]={ 0xc2u, 0x80u };
+        const unsigned char ustr2[]={ 0xc4u, 0x80u };
+        const unsigned char ustr3[]={ 0xdfu, 0xbfu };
+        const unsigned char ustr_er[]={ 0xdfu, 0xc0u };
+        
+        const char* str1 = (const char*)ustr1;
+        const char* str2 = (const char*)ustr2;
+        const char* str3 = (const char*)ustr3;
+        const char* str_er = (const char*)ustr_er;
+        
+        unsigned int codepoint = 0;
+        
+        const char* res = 0;
+
+        codepoint = 0;
+        res = wtf8_decode(str1, 2, &codepoint);
+        should_equal( codepoint, 0x80u);
+        should_equal( res, str1+2 );
+
+        codepoint = 0;
+        res = wtf8_decode(str2, 2, &codepoint);
+        should_equal( codepoint, 0x100u);
+        should_equal( res, str2+2 );
+
+        codepoint = 0;
+        res = wtf8_decode(str3, 2, &codepoint);
+        should_equal( codepoint, 0x7ffu);
+        should_equal( res, str3+2 );
+        
+        codepoint = 0;
+        res = wtf8_decode(str_er, 2, &codepoint);
+        should_equal( codepoint, 0xfffdu);
+        should_equal( res, str_er );
+        
+    }
+
+    it("should decode codepoint from utf8 string in the three octet range 800-ffff") {
+        
+        const unsigned char ustr1[]={ 0xe0u, 0xa0u, 0x80u };
+        const unsigned char ustr2[]={ 0xe1u, 0x80u, 0x80u };
+        const unsigned char ustr3[]={ 0xefu, 0xbfu, 0xbfu };
+        const unsigned char ustr_er[]={ 0xefu, 0xbfu, 0xc0u };
+        
+        const char* str1 = (const char*)ustr1;
+        const char* str2 = (const char*)ustr2;
+        const char* str3 = (const char*)ustr3;
+        const char* str_er = (const char*)ustr_er;
+        
+        unsigned int codepoint = 0;
+        
+        const char* res = 0;
+
+        codepoint = 0;
+        res = wtf8_decode(str1, 3, &codepoint);
+        should_equal( codepoint, 0x800u);
+        should_equal( res, str1+3 );
+
+        codepoint = 0;
+        res = wtf8_decode(str2, 3, &codepoint);
+        should_equal( codepoint, 0x1000u);
+        should_equal( res, str2+3 );
+
+        codepoint = 0;
+        res = wtf8_decode(str3, 3, &codepoint);
+        should_equal( codepoint, 0xffffu);
+        should_equal( res, str3+3 );
+        
+        codepoint = 0;
+        res = wtf8_decode(str_er, 3, &codepoint);
+        should_equal( codepoint, 0xfffdu);
+        should_equal( res, str_er );
+        
+    }
+
+    it("should decode codepoint from utf8 string in the four octet range 10000-1ffff") {
+        
+        const unsigned char ustr1[]={ 0xf0u, 0x90u, 0x80u, 0x80u };
+        const unsigned char ustr2[]={ 0xf0u, 0x92u, 0x80u, 0x80u };
+        const unsigned char ustr3[]={ 0xf0u, 0x9fu, 0xbfu, 0xbfu };
+        const unsigned char ustr_er[]={ 0xf0u, 0x9fu, 0xbfu, 0xc0u };
+        
+        const char* str1 = (const char*)ustr1;
+        const char* str2 = (const char*)ustr2;
+        const char* str3 = (const char*)ustr3;
+        const char* str_er = (const char*)ustr_er;
+        
+        unsigned int codepoint = 0;
+        
+        const char* res = 0;
+
+        codepoint = 0;
+        res = wtf8_decode(str1, 4, &codepoint);
+        should_equal( codepoint, 0x10000u);
+        should_equal( res, str1+4 );
+
+        codepoint = 0;
+        res = wtf8_decode(str2, 4, &codepoint);
+        should_equal( codepoint, 0x12000u);
+        should_equal( res, str2+4 );
+
+        codepoint = 0;
+        res = wtf8_decode(str3, 4, &codepoint);
+        should_equal( codepoint, 0x1ffffu);
+        should_equal( res, str3+4 );
+        
+        codepoint = 0;
+        res = wtf8_decode(str_er, 4, &codepoint);
+        should_equal( codepoint, 0xfffdu);
+        should_equal( res, str_er );
+        
+    }
+
     
 }
 
--- wtf8.h
+++ wtf8.h
@@ -1,34 +1,97 @@
 #ifndef WTF8_WTF8_H
 #define WTF8_WTF8_H
 
-#ifdef __cplusplus
-extern "C" {
+
+#ifdef _WIN32
+#define uint32_t unsigned int
+#else
+#include <stdint.h>
 #endif
 
 
-static inline const char* decode_utf8(const char* str, int maxbytes, unsigned int* result) {
+// Copyright (c) 2008-2010 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+
+// Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#define UTF8_ACCEPT 0
+#define UTF8_REJECT 12
+
+static const uint8_t wtf8_utf8d[] = {
+  // The first part of the table maps bytes to character classes that
+  // to reduce the size of the transition table and create bitmasks.
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+   1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+   7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+   8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+  10,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3, 11,6,6,6,5,8,8,8,8,8,8,8,8,8,8,8,
+
+  // The second part is a transition table that maps a combination
+  // of a state of the automaton and a character class to a state.
+   0,12,24,36,60,96,84,12,12,12,48,72, 12,12,12,12,12,12,12,12,12,12,12,12,
+  12, 0,12,12,12,12,12, 0,12, 0,12,12, 12,24,12,12,12,12,12,24,12,24,12,12,
+  12,12,12,12,12,12,12,24,12,12,12,12, 12,24,12,12,12,12,12,12,12,24,12,12,
+  12,12,12,12,12,12,12,36,12,36,12,12, 12,36,12,12,12,12,12,36,12,36,12,12,
+  12,36,12,12,12,12,12,12,12,12,12,12, 
+};
+
+uint32_t inline wtf8_decode_state(uint32_t* state, uint32_t* codep, uint32_t byte) {
+  uint32_t type = wtf8_utf8d[byte];
+
+  *codep = (*state != UTF8_ACCEPT) ?
+    (byte & 0x3fu) | (*codep << 6) :
+    (0xff >> type) & (byte);
+
+  *state = wtf8_utf8d[256 + *state + type];
+  return *state;
+}
+
+
+
+
+static inline const char* wtf8_decode(const char* str, int maxbytes, uint32_t* result) {
 
     const unsigned char* ustr = (unsigned char*)str;
-    const unsigned int replacement_char = 0xfffd;
-
-    if(maxbytes <= 0) result = 0;
-
-    if(maxbytes >= 1 && ustr[0] <= 0x7f) {
-        *result = ustr[0];
-        return str+1;
+    uint32_t state = 0;
+    while(maxbytes--) {
+        int res = wtf8_decode_state(&state, result, *ustr);
+        ustr++;
+        if(res == UTF8_ACCEPT) return (const char*)ustr;
     }
 
-    *result = replacement_char;
+    *result = 0xfffd;
+    
     return str;
     
 }
 
 
 
-
-
-#ifdef __cplusplus
-}
+#ifdef _WIN32
+#undef uint32_t 
 #endif
+
 
 #endif
